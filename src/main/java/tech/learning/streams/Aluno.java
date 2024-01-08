46e8b612b384bae24194ee8a2977fa786e096ce5
Ivan Silvestre <isilvestreneto@gmail.com>
2024-01-08 20:29:26 -0300

    MinMax

--- src/main/java/tech/learning/streams/Aluno.java
+++ src/main/java/tech/learning/streams/Aluno.java
@@ -4,6 +4,14 @@ public class Aluno {
     final String nome;
     final double nota;
 
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "nome='" + nome + '\'' +
+                ", nota=" + nota +
+                '}';
+    }
+
     public Aluno(String nome, double nota) {
         this.nome = nome;
         this.nota = nota;
